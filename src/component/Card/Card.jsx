d68f7981e831d7bcf8bcf5d5c559bbc101410a8b
bigyam07 <bigyamkarmacharya@gmail.com>
2025-09-12 19:40:07 +0545

    Hover-detail

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -4,14 +4,19 @@ const Card = ({ movie, setMovie }) => {
   return (
     <div className="movie-card-container">
       {
-        movie.map((item) => {
+        movie.map((movie) => {
           return (
             <>
               <div className="movie-card">
                 <div className="fav-icon">
-
+                  <i className="fa-solid fa-heart"></i>
                 </div>
-                <img key={item.id} src={`https://image.tmdb.org/t/p/original/${item.poster_path}`} />
+                <div className="movie-detail">
+                  <h4>{movie.title}</h4>
+                  <p>{movie.overview}</p>
+                  <p><b>Release</b>: {movie.release_date}</p>
+                </div>
+                <img key={movie.id} src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} />
               </div>
             </>
           )
